45d6dcb533f544fcf7b3433287da68127c2eca2e
srijansalian <srijansalian@gmail.com>
2020-03-09 17:00:45 +0530

    [Srijan].Modifed login  in components

--- FundooWeb/src/app/app-routing.module.ts
+++ FundooWeb/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './component/login/login.component';
 
 const routes: Routes = [
   {path:'',component:LoginComponent},
+  {path:'login',component:LoginComponent},
   {path:'register',component:RegisterComponent}
 ];
 
