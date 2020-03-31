b5a768ba74c72c171ae8f02c3e2d729be8863935
srijansalian <srijansalian@gmail.com>
2020-03-31 12:37:20 +0530

    [Srijan].Updated in the getservice in the display note

--- FundooWeb/src/app/component/displaynotes/displaynotes.component.ts
+++ FundooWeb/src/app/component/displaynotes/displaynotes.component.ts
@@ -17,6 +17,8 @@ export class DisplaynotesComponent implements OnInit {
   respo
   pined=new Array<Note>();
   others=new Array<Note>();
+  archive=new Array<Note>();
+  trash=new Array<Note>();
   displayNotes=new Array<Note>();
   note=new Note;
 
@@ -32,17 +34,23 @@ export class DisplaynotesComponent implements OnInit {
               this.displayNotes=this.respo;
              this. pined=[];
               this.others=[];
-              console.log(this.displayNotes.filter(note=>note.noteid===1));
-              console.log(this.displayNotes.filter(note=>note.title==="Introduction"));
-              console.log(this.displayNotes.filter(note=>note.isPinned===false));
-              console.log(this.displayNotes.filter(note=>note.isArchived===false));
-              console.log(this.displayNotes.filter(note=>note.isPinned===false&&note.isArchived===false&&note.isTrashed===false));
+              this.archive=[];
+              this.trash=[];
+              // console.log(this.displayNotes.filter(note=>note.noteid===1));
+              // console.log(this.displayNotes.filter(note=>note.title==="Introduction"));
+              // console.log(this.displayNotes.filter(note=>note.isPinned===false));
+              // console.log(this.displayNotes.filter(note=>note.isArchived===false));
+              //console.log(this.displayNotes.filter(note=>note.isPinned===false&&note.isArchived===false&&note.isTrashed===false));
               this.displayNotes.filter(note=>note.isPinned===false&&note.isArchived===false&&note.isTrashed===false).map(note=>this.others.push(note));
               this.displayNotes.filter(note=>note.isPinned===true&&note.isArchived===false&&note.isTrashed===false).map(note=>this.pined.push(note)); 
+              this.displayNotes.filter(note=>note.isPinned===false&&note.isArchived===true&&note.isTrashed===false).map(note=>this.archive.push(note));
+              this.displayNotes.filter(note=>note.isTrashed===true).map(note=>this.trash.push(note)); 
               // this.noteDetails=noteData;
                
                console.log(this.others);
                console.log(this.pined);
+               console.log(this.archive);
+               console.log(this.trash);
               //  console.log(noteData);
               //  console.log(this.respo);
                
